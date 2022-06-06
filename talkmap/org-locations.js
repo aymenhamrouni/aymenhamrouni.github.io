97071f5c6e78c934c80e41d0e6816a458363555f
Aymen Hamrouni <aymen.hamrouni@supcom.tn>
2022-06-07 01:23:31 +0300

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,20 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+    "IEEE MWSCAS 2019",
+  
+  32.77824220252471, -96.79747052216919],
+  ["IEEE ICM 2019",30.043616, 31.281635],
+  
+   ["IEEE TEMSCON 2020",42.367779, -83.059363],
+  
+  
+   ["IEEE GCAIoT 2020",30.838768, 28.950383],
+  
+   ["IEEE MWSCAS 2022",33.582638, 130.353372],
+  
+   ["Stevens Institute of Technology",22.310020840923368, 39.104739915346464],
+  
+  
+   ["King Abdullah University of Science and Technologies",22.310020840923368, 39.104739915346464],
+  
 ];
